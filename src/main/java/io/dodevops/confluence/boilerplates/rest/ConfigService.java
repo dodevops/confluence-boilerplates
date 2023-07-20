fa6b36753039871483440611af6d5aa9b9b2e400
Dennis Ploeger <dennis.ploeger@kps.com>
2023-07-20 07:57:01 +0200

    fix: Missing Inject constructor

--- src/main/java/io/dodevops/confluence/boilerplates/rest/ConfigService.java
+++ src/main/java/io/dodevops/confluence/boilerplates/rest/ConfigService.java
@@ -2,6 +2,7 @@ package io.dodevops.confluence.boilerplates.rest;
 
 import io.dodevops.confluence.boilerplates.config.BoilerplatesConfigurationManager;
 
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -13,6 +14,11 @@ import javax.ws.rs.core.Response;
 @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 public class ConfigService {
     private BoilerplatesConfigurationManager boilerplatesConfigurationManager;
+
+    @Inject
+    public ConfigService(final BoilerplatesConfigurationManager boilerplatesConfigurationManager) {
+        this.boilerplatesConfigurationManager = boilerplatesConfigurationManager;
+    }
 
     @GET
     public Response getConfig() {
